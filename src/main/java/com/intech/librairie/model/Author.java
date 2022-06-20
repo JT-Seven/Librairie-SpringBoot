71be194c34f01a17a5b2369d1857401a26cda568
Steven <terneus@et.intechinfo.fr>
2022-06-20 19:20:55 +0200

    git init

--- src/main/java/com/intech/librairie/model/Author.java
+++ src/main/java/com/intech/librairie/model/Author.java
@@ -7,19 +7,20 @@ import lombok.Setter;
 
 import javax.persistence.*;
 
-@Entity
-@Table(name = "auhtor")
+@Entity(name = "auhtor")
 @Getter
 @Setter
 @NoArgsConstructor
 public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    int id;
+    private int id;
 
-    @Column(name = "author_name")
+    @Column(name = "author_firstname")
     private String firstName;
+    @Column(name = "author_lastname")
     private String lastName;
+    @Column(name = "author_date_of_birth")
     private String dateOfBirth;
 
     public Author(String name, String lastName, String dateOfBirth) {
